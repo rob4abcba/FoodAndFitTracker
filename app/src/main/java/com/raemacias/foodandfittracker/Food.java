0568a9c40a8db2f4f8136b22bd1cdf6e98048409
Rae <raemmacias@gmail.com>
2018-10-28 18:44:50 -0600

    Added menu and strings and layouts.

--- app/src/main/java/com/raemacias/foodandfittracker/Food.java
+++
@@ -1,7 +0,0 @@
-package com.raemacias.foodandfittracker;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class Food extends AppCompatActivity{
-
-}
--- app/src/main/java/network/TrackerInterface.java
+++ app/src/main/java/network/TrackerInterface.java
@@ -2,8 +2,9 @@ package network;
 
 import com.raemacias.foodandfittracker.BuildConfig;
 
+import java.util.List;
+
 import models.getCaloriesBurnedForExercises.Exercise;
-import models.getFoodsNutrients.Food;
 import models.searchFoods.Common;
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -14,7 +15,6 @@ import retrofit2.http.Url;
 public interface TrackerInterface {
 
     //The interface defines the possible HTTP operations
-
     //define base URL
 
     //all of the names and keys below need to be updated to the tracker app
@@ -26,36 +26,18 @@ public interface TrackerInterface {
     String EXERCISE = "exercise";
     String WEIGHT = "weigh_in";
 
-    //http request - adds to end of base url
-    //add a method - call <type> e.g. array would be list,
-    // type of list, list of type movie
-    //can name get anything I want
-//    @Headers("user-key: API_KEY")
-//    @Headers("Content-Type:application/json, APP_ID:NutritionixAppID, API_KEY:NutritionixAppKey")
-//    @GET("/natural/nutrients")
-//    Call<Food> getFoodName(
-//            @Query("api_key") String api_key);
-
     //searchFoods
-    @Headers("Content-Type:application/json, APP_ID:NutritionixAppID, API_KEY:NutritionixAppKey")
+    @Headers("APP_ID:NutritionixAppID, API_KEY:NutritionixAppKey")
     @GET("/search/instant")
-    Call<Common> getFoodName (
+    Call<Common> getFoodName(
             @Query("api_key") String api_key);
 
-    @Headers("Content-Type:application/json, APP_ID:NutritionixAppID, API_KEY:NutritionixAppKey")
+    //searchExercises
+    @Headers("APP_ID:NutritionixAppID, API_KEY:NutritionixAppKey")
     @GET("/natural/exercise")
-    Call<Exercise> getName (
-            @Query("api_key") String api_key);
+    Call<Exercise> getName();
 
     @Headers("Content-Type:application/json, APP_ID:NutritionixAppID, API_KEY:NutritionixAppKey")
     @GET()
-    Call<String>getStringResponse(@Url String url);
-
-
-
-//    @GET("{movie_id}/reviews")
-//    Call<ReviewModel> getReviewList(@Path("movie_id") int id, @Query("api_key") String api_key);
-//
-//    @GET("{movie_id}/videos")
-//    Call<TrailerModel> getMovieTrailer(@Path("movie_id") int id, @Query("api_key") String api_key);
+    Call<String> getStringResponse(@Url String url);
 }
