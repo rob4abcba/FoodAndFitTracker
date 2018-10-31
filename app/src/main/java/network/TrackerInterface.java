e3f30cfb2da50603f43827edcccb1b05a3efc269
Rae <raemmacias@gmail.com>
2018-10-31 08:44:46 -0600

    Another attempt at Retrofit - still no data.

--- app/src/main/java/network/TrackerInterface.java
+++ app/src/main/java/network/TrackerInterface.java
@@ -2,17 +2,21 @@ package network;
 
 import com.raemacias.foodandfittracker.BuildConfig;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import models.getCaloriesBurnedForExercises.Exercise;
 import models.getCaloriesBurnedForExercises.ExerciseRequest;
 import models.searchFoods.Common;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
+
 
 public interface TrackerInterface {
 
@@ -36,11 +40,18 @@ public interface TrackerInterface {
 
     //searchExercises
 
-//    @FormUrlEncoded
-    @Headers({"Content-Type: application/json", "x-app-id:c9a9b2dd, x-app-key:xxxxx"})
-    @POST("v2/natural/exercise")
-    Call<ExerciseRequest> getQuery(@Body String query);
+////    @FormUrlEncoded
+//    @Headers({"Content-Type: application/json", "x-app-id:c9a9b2dd, x-app-key:xxxxx"})
+//    @POST("v2/natural/exercise")
+//    Call<ExerciseRequest> getQuery(@Body String query);
 //    @Field("query") String query);
+
+
+    //using request body
+//    @FormUrlEncoded
+    @Headers({"Content-Type: application/json", "x-app-id:c9a9b2dd, x-app-key:xxx"})
+    @POST("v2/natural/exercise")
+    Call<Exercise> getStringScalar(@Body ExerciseRequest body);
 
 
 //            @Field("gender") String gender);
@@ -55,5 +66,8 @@ public interface TrackerInterface {
 //    @Field("name") String name);
 //            @Path("query") String query);
 //    Call<List<Exercise>> search(@Query("api_key") String api_key);
+
+
+
 
 }
